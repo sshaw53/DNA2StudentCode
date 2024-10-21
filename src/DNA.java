6f0e2840de32ae5b0cc87c20e7cb46519da21b29
sierrashaw <sierra.shaw@menloschool.org>
2024-10-20 19:21:00 -0700

    Implemented hash functions, still too slow :(

--- src/DNA.java
+++ src/DNA.java
@@ -1,4 +1,5 @@
 import static java.lang.Math.pow;
+import static jdk.internal.org.jline.utils.Colors.h;
 
 /**
  * DNA
@@ -13,16 +14,21 @@ import static java.lang.Math.pow;
  */
 
 public class DNA {
+    // Typically, a radix would be 256 if dealing with characters but since our alphabet is just A,C,T,G we only need
+    // a radix of 4 to solve this, and p is typically a large prime number
+    public static int RADIX = 4;
+    public static int p = 9737333;
+
     public static int STRCount(String sequence, String STR) {
         int max = 0;
         int seq_len = sequence.length();
         int strlen = STR.length();
-        int RADIX = 4;
-        // Assigning integer values for all different letters to help create a constant-time check
-        int num_STR = hash(STR, RADIX, strlen);
-        int num_current = hash(sequence, RADIX, strlen);
 
-        // Go through each character and find every instance of the first letter of the STR in the sequence
+        // Assigning integer values for the STR and the beginning of the sequence to help create a constant-time check
+        int num_STR = hash(STR, strlen, 0);
+        int num_current = hash(sequence, strlen, 0);
+
+        // Go through each character and look for every instance where the STR matches the sequence
         for (int i = 0; i < seq_len; i++) {
             // When we find a match, call the find() function to skip ahead and look for number of repeats
             if (num_STR == num_current) {
@@ -35,20 +41,7 @@ public class DNA {
 
             // Create the numerical version of the next chunk (cut off the max number, add the next number)
             if (i + strlen < seq_len) {
-                num_current = num_current % (int) Math.pow(10, strlen - 1);
-                num_current *= 10;
-                if (sequence.charAt(i + strlen) == 'A') {
-                    num_current += a;
-                }
-                else if (sequence.charAt(i + strlen) == 'G') {
-                    num_current += g;
-                }
-                else if (sequence.charAt(i + strlen) == 'C') {
-                    num_current += c;
-                }
-                else if (sequence.charAt(i + strlen) == 'T') {
-                    num_current += t;
-                }
+                num_current = rehash(num_current, sequence, strlen, i);
             }
         }
         return max;
@@ -57,171 +50,37 @@ public class DNA {
     public static int find(int idx, String seq, int num_STR, int n_current, int strlen, int seq_len) {
         int current = idx;
         int finds = 0;
-        // Assigning integer values for all different letters to help create a constant-time check
-        int a = 1;
-        int g = 2;
-        int c = 3;
-        int t = 4;
         int num_current = n_current;
 
         // If we haven't gotten to the end of the String and the next characters align with the STR, continue to traverse
         // and increase finds by 1
-        while (current + strlen < seq_len && num_current == num_STR) {
+        while (current + 2 * strlen < seq_len && num_current == num_STR) {
             current += strlen;
             finds += 1;
 
-            num_current = 0;
             // Now shift the characters to find the number value of the next STR-length chunk
-            for (int i = 0; i < strlen; i++) {
-                if (seq.charAt(i + current) == 'A') {
-                    num_current += a;
-                }
-                else if (seq.charAt(i + current) == 'G') {
-                    num_current += g;
-                }
-                else if (seq.charAt(i + current) == 'C') {
-                    num_current += c;
-                }
-                else if (seq.charAt(i + current) == 'T') {
-                    num_current += t;
-                }
-                if (i != strlen - 1) {
-                    num_current *= 10;
-                }
-            }
+            num_current = hash(seq, strlen, current);
         }
 
-        // If not, it will immediately return false
+        // If not, it will immediately exit the loop and return the number of finds
         return finds;
     }
 
-    public static int hash(String str, int radix, int len) {
-        //
-        return 0;
+    public static int hash(String str, int len, int idx) {
+        // Modifies a string to a unique number utilizing hash functions (Horner's method)
+        int hashed = 0;
+        for(int i = idx; i < idx + len; i++) {
+            hashed = (RADIX * hashed + str.charAt(i)) % p;
+        }
+        return hashed;
     }
 
-    public static int rehash(String str, int radix) {
-        //
-        return 0;
+    public static int rehash(int toHash, String str, int len, int idx) {
+        // Adds the next letter, using the Rabin-Karp fingerprinting algorithm
+        // Need to subtract the first term, multiply by R, and add the next term
+        int rehashed;
+        rehashed = ((toHash + p) - str.charAt(idx) * (int)Math.pow(RADIX, len - 1) % p) % p;
+        rehashed = ((rehashed * RADIX) + str.charAt(idx + len)) % p;
+        return rehashed;
     }
-
-    /* public static int STRCount(String sequence, String STR) {
-        int max = 0;
-        int seq_len = sequence.length();
-        int strlen = STR.length();
-        // Assigning integer values for all different letters to help create a constant-time check
-        int a = 1;
-        int g = 2;
-        int c = 3;
-        int t = 4;
-        int num_STR = 0;
-        int num_current = 0;
-
-        // Convert both the STR and the first chunk in the seq of length STR to a unique number value to then
-        // compare in constant time
-        for (int i = 0; i < strlen; i++) {
-            if (Character.toUpperCase(STR.charAt(i)) == 'A') {
-                num_STR += a;
-            }
-            else if (Character.toUpperCase(STR.charAt(i)) == 'G') {
-                num_STR += g;
-            }
-            else if (Character.toUpperCase(STR.charAt(i)) == 'C') {
-                num_STR += c;
-            }
-            else if (Character.toUpperCase(STR.charAt(i)) == 'T') {
-                num_STR += t;
-            }
-
-            if (Character.toUpperCase(sequence.charAt(i)) == 'A') {
-                num_current += a;
-            }
-            else if (Character.toUpperCase(sequence.charAt(i)) == 'G') {
-                num_current += g;
-            }
-            else if (Character.toUpperCase(sequence.charAt(i)) == 'C') {
-                num_current += c;
-            }
-            else if (Character.toUpperCase(sequence.charAt(i)) == 'T') {
-                num_current += t;
-            }
-            if (i != strlen - 1) {
-                num_STR *= 10;
-                num_current *= 10;
-            }
-        }
-
-        // Go through each character and find every instance of the first letter of the STR in the sequence
-        for (int i = 0; i < seq_len; i++) {
-            // When we find a match, call the find() function to skip ahead and look for number of repeats
-            if (num_STR == num_current) {
-                int current_reps = find(i, sequence, num_STR, num_current, strlen, seq_len);
-                // If we find a larger value, change the max index
-                if (current_reps > max) {
-                    max = current_reps;
-                }
-            }
-
-            // Create the numerical version of the next chunk (cut off the max number, add the next number)
-            if (i + strlen < seq_len) {
-                num_current = num_current % (int) Math.pow(10, strlen - 1);
-                num_current *= 10;
-                if (sequence.charAt(i + strlen) == 'A') {
-                    num_current += a;
-                }
-                else if (sequence.charAt(i + strlen) == 'G') {
-                    num_current += g;
-                }
-                else if (sequence.charAt(i + strlen) == 'C') {
-                    num_current += c;
-                }
-                else if (sequence.charAt(i + strlen) == 'T') {
-                    num_current += t;
-                }
-            }
-        }
-        return max;
-    }
-
-    public static int find(int idx, String seq, int num_STR, int n_current, int strlen, int seq_len) {
-        int current = idx;
-        int finds = 0;
-        // Assigning integer values for all different letters to help create a constant-time check
-        int a = 1;
-        int g = 2;
-        int c = 3;
-        int t = 4;
-        int num_current = n_current;
-
-        // If we haven't gotten to the end of the String and the next characters align with the STR, continue to traverse
-        // and increase finds by 1
-        while (current + strlen < seq_len && num_current == num_STR) {
-            current += strlen;
-            finds += 1;
-
-            num_current = 0;
-            // Now shift the characters to find the number value of the next STR-length chunk
-            for (int i = 0; i < strlen; i++) {
-                if (seq.charAt(i + current) == 'A') {
-                    num_current += a;
-                }
-                else if (seq.charAt(i + current) == 'G') {
-                    num_current += g;
-                }
-                else if (seq.charAt(i + current) == 'C') {
-                    num_current += c;
-                }
-                else if (seq.charAt(i + current) == 'T') {
-                    num_current += t;
-                }
-                if (i != strlen - 1) {
-                    num_current *= 10;
-                }
-            }
-        }
-
-        // If not, it will immediately return false
-        return finds;
-    } */
-
 }
